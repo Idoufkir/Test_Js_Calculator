a6fae9ea78b6377a636842e0a2f04dc92a0ee9b9
idoufkir <mustafa.idoufkir@gmail.com>
2020-01-10 01:11:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+var n1;
+var n2;
+
+function Addition(n1,n2){
+	document.getElementById('Result').innerHTML = parseFloat(n1) + parseFloat(n2);
+}
+
+
+function Soustraction(n1,n2){
+	document.getElementById('Result').innerHTML = parseFloat(n1) - parseFloat(n2);
+}
+
+
+function Multiplication(n1,n2){
+	document.getElementById('Result').innerHTML = parseFloat(n1) * parseFloat(n2);
+}
+
+
+function Division(n1,n2){
+	document.getElementById('Result').innerHTML = parseFloat(n1) / parseFloat(n2);
+}
+
